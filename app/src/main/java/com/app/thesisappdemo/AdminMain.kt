6a1430c6ef4107d03ed4710310630afb127a5a00
WilliamHoops2 <fairytail_william@yahoo.com>
2023-05-21 00:17:30 +0700

    Halaman admin main + transaction, operational bottom menu

---
+++ app/src/main/java/com/app/thesisappdemo/AdminMain.kt
@@ -0,0 +1,39 @@
+package com.app.thesisappdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.app.thesisappdemo.databinding.AdminMainBinding
+
+class AdminMain : AppCompatActivity() {
+
+    private lateinit var binding: AdminMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = AdminMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(admin_home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.admin_home -> replaceFragment(admin_home())
+                R.id.admin_transaction -> replaceFragment(admin_transaction())
+
+                else -> {
+
+                }
+
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
