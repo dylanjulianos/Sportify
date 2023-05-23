545da37158c9d06fc1183f0c9b3185ce759264fa
WilliamHoops2 <fairytail_william@yahoo.com>
2023-05-23 16:13:15 +0700

    tambah add item button, log out button

---
+++ app/src/main/java/com/app/thesisappdemo/Items.kt
@@ -0,0 +1,11 @@
+package com.app.thesisappdemo
+
+data class Items (
+    var item_name: String,
+    var item_type: String,
+    var item_image_link: String,
+    var sport_category: String,
+    var price: Float,
+    var rent_duration: Int,
+    var item_detail: String
+)
