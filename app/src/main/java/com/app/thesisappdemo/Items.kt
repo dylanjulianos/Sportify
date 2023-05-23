45da539ad515703c5a768c6dd95e68500285b3c8
WilliamHoops2 <fairytail_william@yahoo.com>
2023-05-23 23:27:40 +0700

    add item to firestore (belum final, masih error)

--- app/src/main/java/com/app/thesisappdemo/Items.kt
+++ app/src/main/java/com/app/thesisappdemo/Items.kt
@@ -2,10 +2,7 @@ package com.app.thesisappdemo
 
 data class Items (
     var item_name: String,
-    var item_type: String,
-    var item_image_link: String,
-    var sport_category: String,
     var price: Float,
-    var rent_duration: Int,
-    var item_detail: String
+    var sport_category: String,
+    var item_bio: String
 )
--- app/src/main/java/com/app/thesisappdemo/admin_add_item.kt
+++ app/src/main/java/com/app/thesisappdemo/admin_add_item.kt
@@ -1,12 +1,23 @@
 package com.app.thesisappdemo
 
+import android.content.ClipData.Item
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,16 +41,48 @@ class admin_add_item : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
-
+    private val itemCollectionRef = Firebase.firestore.collection("Items")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         val v = inflater.inflate(R.layout.fragment_admin_add_item, container, false)
 
+        val save_item_button = v.findViewById(R.id.save_item_button) as Button
+        val item_name = v.findViewById(R.id.item_name) as EditText
+        val item_price = v.findViewById(R.id.item_price) as EditText
+        val item_sport_type = v.findViewById(R.id.item_sport_type) as EditText
+        val item_bio = v.findViewById(R.id.item_description) as EditText
+
+        save_item_button.setOnClickListener{
+            val item_name_input = item_name.text.toString()
+            val item_price_input = item_name.text.toString().toFloat()
+            val item_sport_type_input = item_name.text.toString()
+            val item_bio_input = item_name.text.toString()
+            val item = Items(item_name_input, item_price_input, item_sport_type_input, item_bio_input)
+            saveItem(item)
+        }
+
         return v
+
     }
+
+    private fun saveItem (items: Items) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            itemCollectionRef.add(items).await()
+            withContext(Dispatchers.Main){
+                Toast.makeText(activity, "Successfully Added Item", Toast.LENGTH_LONG).show()
+            }
+        }catch (e: Exception){
+            withContext(Dispatchers.Main){
+                Toast.makeText(activity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
 
     companion object {
         /**
