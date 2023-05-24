df11266d9cf3e8fe661567d1ae6f5407903619bb
hanzelanjaya <samudera699@gmail.com>
2023-05-24 13:29:06 +0700

    redirect button

--- app/src/main/java/com/app/thesisappdemo/MainCustomer.kt
+++ app/src/main/java/com/app/thesisappdemo/MainCustomer.kt
@@ -1,27 +1,31 @@
 package com.app.thesisappdemo
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.app.thesisappdemo.databinding.MainCustomerBinding
+import kotlinx.coroutines.flow.merge
 
 class MainCustomer : AppCompatActivity() {
-
     private lateinit var binding : MainCustomerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainCustomerBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(HomeFragment())
-
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.home -> replaceFragment(HomeFragment())
                 R.id.cart -> replaceFragment(CartFragment())
                 R.id.transaction -> replaceFragment(TransactionFragment())
-
                 else-> {
-
                 }
             }
             true
